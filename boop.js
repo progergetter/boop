e5bae184348c67425d41c0d1509414e6b11c1a16
igoodmood <vladimirscherbina60@gmail.com>
2024-02-12 20:42:58 +0200

    implemented merge of kittens

--- boop.js
+++ boop.js
@@ -3,7 +3,8 @@ document.body.onload = init;
 const n = 6;
 const m = 6;
 let index = 0;
-const kittenCounter = document.getElementById("tool");
+const kittenCounter = document.getElementById("kittens");
+const catsCounter = document.getElementById("cats");
 const color = generateColor();
 class Cell {
   id;
@@ -60,8 +61,22 @@ function shift(coord) {
   let x = coord - y * m;
   cells[y][x].kitten = true;
   cells[y][x].block.classList.add("kitten");
-  for (let i = 0; i < dirs.length; i++) {
-    dfs(y, x, 0, i);
+  let tripleFound = false;
+  let points = [];
+  for (let i = 0; tripleFound === false && i < dirs.length; i++) {
+    let res = checkTriple(y, x, 0, i);
+    tripleFound = tripleFound || res;
+  }
+  if (tripleFound === false) {
+    for (let i = 0; i < dirs.length; i++) {
+      dfs(y, x, 0, i, points);
+    }
+    for (let i = 0; i < points.length; i++) {
+      for (let j = 0; tripleFound === false && j < dirs.length; j++) {
+        let res = checkTriple(points[i][0], points[i][1], 0, j);
+        tripleFound = tripleFound || res;
+      }
+    }
   }
   kittenCounter.innerText = `Kittens: ${
     document.getElementsByClassName("kitten").length
@@ -79,13 +94,31 @@ const dirs = [
   [-1, -1],
 ];
 
-function dfs(x, y, len, directionIndex) {
+function checkTriple(x, y, len, directionIndex) {
+  if (len === 3) return true;
+  if (x < 0 || x === n || y < 0 || y === m) return false;
+  if (len > 0 && cells[x][y].kitten === false) return false;
+  let res = checkTriple(
+    x + dirs[directionIndex][0],
+    y + dirs[directionIndex][1],
+    len + 1,
+    directionIndex
+  );
+  if (res === true) {
+    cells[x][y].kitten = false;
+    cells[x][y].block.classList.remove("kitten");
+  }
+  return res;
+}
+
+function dfs(x, y, len, directionIndex, points) {
   if (x < 0 || x === n || y < 0 || y === m || len == 3) return true;
   if (len === 1 && cells[x][y].kitten === false) return false;
   if (len === 2) {
     if (cells[x][y].kitten === true) return false;
     cells[x][y].kitten = true;
     cells[x][y].block.classList.add("kitten");
+    points.push([x, y]);
     return true;
   }
   if (
@@ -93,7 +126,8 @@ function dfs(x, y, len, directionIndex) {
       x + dirs[directionIndex][0],
       y + dirs[directionIndex][1],
       len + 1,
-      directionIndex
+      directionIndex,
+      points
     )
   ) {
     if (len === 1) {
