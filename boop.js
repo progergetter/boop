22bff83557c944c8d07d46cbe20979296aeb82f9
igoodmood <vladimirscherbina60@gmail.com>
2024-02-16 20:18:42 +0200

    Fixed dfs calculations, added cats, fixed numerous bugs

--- boop.js
+++ boop.js
@@ -1,11 +1,28 @@
 document.body.onload = init;
 
+let index = 0;
+let turn = 1;
+let cats1 = 0;
+let cats2 = 0;
+let kittenLimit = 8;
+let catLimit = 3;
+let cells = [];
 const n = 6;
 const m = 6;
-let index = 0;
 const kittenCounter = document.getElementById("kittens");
 const catsCounter = document.getElementById("cats");
 const color = generateColor();
+const dirs = [
+  [0, 1],
+  [1, 0],
+  [-1, 0],
+  [0, -1],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+];
+
 class Cell {
   id;
   block;
@@ -16,6 +33,7 @@ class Cell {
     this.id = index++;
     this.color = "#" + color;
     this.kitten = false;
+    this.cat = false;
   }
 }
 
@@ -23,7 +41,10 @@ function generateColor() {
   return Math.floor(Math.random() * 16777215).toString(16);
 }
 
-let cells = [];
+function changeTurn() {
+  if (turn === 1) turn = 2;
+  else turn = 1;
+}
 
 function init() {
   const mainBlock = document.getElementById("board");
@@ -39,10 +60,26 @@ function init() {
       cell.block = td;
       td.setAttribute("bgcolor", cell.color);
       td.setAttribute("id", cell.id);
+      // left click
       td.addEventListener("click", function () {
-        this.classList.add("kitten1");
+        this.classList.add("kitten" + turn);
         shift(Number(this.id));
+        changeTurn();
       });
+      // right click
+      td.addEventListener(
+        "contextmenu",
+        function (ev) {
+          ev.preventDefault();
+          if (turn === 1 ? cats1 > 0 : cats2 > 0) {
+            this.classList.add("cat" + turn);
+            shift(Number(this.id));
+            changeTurn();
+          }
+          return false;
+        },
+        false
+      );
       tr.appendChild(td);
     }
     cells.push(row);
@@ -52,50 +89,57 @@ function init() {
   mainBlock.appendChild(table);
 }
 
+function updateKittensCounter() {
+  let player1Kittens = `Player's 1 Kittens: ${
+    document.getElementsByClassName("kitten1").length
+  } / ${kittenLimit}`;
+  let player2Kittens = `Player's 2 Kittens: ${
+    document.getElementsByClassName("kitten2").length
+  } / ${kittenLimit}`;
+  kittenCounter.innerText = player1Kittens + "\n" + player2Kittens;
+}
+
+function updateCatsCounter() {
+  let player1Cats = `Player's 1 Cats: ${cats1} / ${catLimit}`;
+  let player2Cats = `Player's 2 Cats: ${cats2} / ${catLimit}`;
+  catsCounter.innerText = player1Cats + "\n" + player2Cats;
+}
+
 function shift(coord) {
   let x = Math.floor(coord / n);
   let y = coord - x * m;
   cells[x][y].kitten = true;
-  cells[x][y].block.classList.add("kitten1");
-  let tripleFound = false;
-  tripleHelper(tripleFound);
+  cells[x][y].block.classList.add("kitten" + turn);
+  let tripleFound = tripleHelper();
   if (tripleFound === false) {
     for (let i = 0; i < dirs.length; i++) {
-      dfs(x, y, 0, i);
+      dfs(x, y, 0, i, "");
     }
-    tripleHelper(tripleFound);
+    tripleHelper();
   }
-  kittenCounter.innerText = `Kittens: ${
-    document.getElementsByClassName("kitten1").length
-  }`;
+  updateKittensCounter();
 }
 
-function tripleHelper(tripleFound) {
+function tripleHelper() {
+  let res = false;
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-      for (let k = 0; tripleFound === false && k < dirs.length; k++) {
-        let res = checkTriple(i, j, 0, k);
-        tripleFound = tripleFound || res;
+      for (let k = 0; k < dirs.length; k++) {
+        res = res || checkTriple(i, j, 0, k);
       }
     }
   }
+  return res;
 }
 
-const dirs = [
-  [0, 1],
-  [1, 0],
-  [-1, 0],
-  [0, -1],
-  [1, 1],
-  [1, -1],
-  [-1, 1],
-  [-1, -1],
-];
-
 function checkTriple(x, y, len, directionIndex) {
-  if (len === 4) return true;
+  if (len === 3) return true;
   if (x < 0 || x === n || y < 0 || y === m) return false;
-  if (len > 0 && cells[x][y].kitten === false) return false;
+  if (
+    cells[x][y].kitten === false ||
+    cells[x][y].block.classList.contains("kitten" + turn) === false
+  )
+    return false;
   let res = checkTriple(
     x + dirs[directionIndex][0],
     y + dirs[directionIndex][1],
@@ -103,32 +147,44 @@ function checkTriple(x, y, len, directionIndex) {
     directionIndex
   );
   if (res === true) {
+    if (len === 0) {
+      if (turn === 1) cats1++;
+      else cats2++;
+      updateCatsCounter();
+    }
     cells[x][y].kitten = false;
-    cells[x][y].block.classList.remove("kitten1");
+    cells[x][y].block.classList.remove("kitten" + turn);
   }
   return res;
 }
 
-function dfs(x, y, len, directionIndex) {
+function dfs(x, y, len, directionIndex, turn) {
   if (x < 0 || x === n || y < 0 || y === m || len == 3) return true;
   if (len === 1 && cells[x][y].kitten === false) return false;
   if (len === 2) {
     if (cells[x][y].kitten === true) return false;
     cells[x][y].kitten = true;
-    cells[x][y].block.classList.add("kitten1");
+    cells[x][y].block.classList.add("kitten" + turn);
     return true;
   }
+  let currTurn =
+    len === 1
+      ? cells[x][y].block.classList.contains("kitten1") === true
+        ? 1
+        : 2
+      : turn;
   if (
     dfs(
       x + dirs[directionIndex][0],
       y + dirs[directionIndex][1],
       len + 1,
-      directionIndex
+      directionIndex,
+      currTurn
     )
   ) {
     if (len === 1) {
       cells[x][y].kitten = false;
-      cells[x][y].block.classList.remove("kitten1");
+      cells[x][y].block.classList.remove("kitten" + currTurn);
     }
   }
   return true;
